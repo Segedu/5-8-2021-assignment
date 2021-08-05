e4557cd9b1698dc5529291aaba9adaf719374f8c
segedu <seggedu20@gmail.com>
2021-08-05 18:45:59 +0300

    question 1-14

---
+++ publish/main.js
@@ -0,0 +1,143 @@
+//question 1
+
+// var firstNum = Number(prompt("write a number")),
+//   secondNum = Number(prompt("write a number"));
+// console.log(firstNum, secondNum);
+
+// var sum = firstNum + secondNum;
+// if (firstNum && secondNum && sum >= 13) {
+//   console.log("bar-mitzva", sum);
+// } else if (sum == 12) {
+//   console.log("bat-mitzva", sum);
+// } else {
+//   console.log("kid", sum);
+// }
+
+//question2
+
+// var startNum = 1;
+// while (startNum <= 100) {
+//   console.log(startNum);
+//   startNum++;
+// }
+
+// question3
+
+// var str = "no ",
+//   userInput = Number(prompt("write a number")),
+//   multistr = str.repeat(userInput);
+// console.log(multistr);
+
+// question4
+
+// for (i = 1; i <= 1000; i++) {
+//   console.log("javascript is not JAVA");
+// }
+
+// question5
+
+// var userName = prompt("write your name"),
+//   userAge = Number(prompt("write your age"));
+// while (userAge) {
+//   console.log(userName.repeat(userAge));
+//   break;
+// }
+
+// question6
+
+// var userName = prompt("write your name"),
+//   userEmail = prompt("write your Email"),
+//   userAge = Number(prompt("write your age"));
+// console.log(userName, userEmail, userAge);
+
+// var userNewNum = Number(prompt("write a number"));
+// var allData = userName + userEmail + userAge;
+// for (var i = 1; i <= userNewNum; i++ && userAge > 18) {
+//   console.log(allData.repeat(userNewNum));
+// }
+
+//question7
+
+// var userNumInput = Number(prompt("write a number")),
+//   myName = "segedu",
+//   userNameInput = prompt("write a name");
+
+// for (var i = 0; i < userNumInput; i++) {
+//   //   console.log(userInput);
+//   if (userNameInput == myName) {
+//     console.log("hey boss " + myName);
+//   } else {
+//     console.log("no entry");
+//   }
+// }
+
+//question8
+
+// var firstNum = Number(prompt("write a number")),
+//   secondNum = Number(prompt("write a number again please"));
+
+// for (var i = 0; i < firstNum; i++) {
+//   for (var j = 0; j < secondNum; j++) {
+//     console.log(firstNum * secondNum);
+//   }
+// }
+
+//question9
+
+// var numArray = [3, 6, 12, 4, 8];
+// console.log(numArray);
+
+//question10
+
+// var strArray = ["Noa", "Dana", "Ben", "Yaara", "yosef"];
+// console.log(strArray);
+
+//question11
+// var numArray = [10, 12, 18, 21, 25, 29, 34, 50],
+//   equalArray = [];
+// for (var i = 0; i < numArray.length; i++) {
+//   if (numArray[i] % 2 == 0) {
+//     equalArray.push(numArray[i]);
+//   }
+// }
+// console.log(equalArray);
+
+//question12
+
+// var fiveNumsArray = [];
+// for (var i = 0; i < 5; i++) {
+//   var userInput = Number(prompt("put a number"));
+//   fiveNumsArray.push(userInput);
+// }
+// console.log(fiveNumsArray);
+
+//question13
+
+// var sevenNumsArray = [], notEven = [];
+// for (var i = 0; i < 7; i++) {
+//   var userInput = Number(prompt("write a number"));
+//   sevenNumsArray.push(userInput);
+//   if (sevenNumsArray[i] % 2) {
+//     notEven.push(userInput);
+//   }
+// }
+// console.log(notEven);
+
+//question14
+
+// var randomArray = [];
+// for (var i = 0; i < 6; i++) {
+//   var userInput = prompt("write somthing");
+//   randomArray.push(userInput);
+
+//   if (!Number(randomArray[i])) {
+//     randomArray[i] = "not a number";
+//   }
+// }
+// console.log(randomArray);
+
+//question15
+
+//question16
+
+//question17
